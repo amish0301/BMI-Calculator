32acd888992b89e16ae8ad15998ed8a2da3d308c
amish <pithvaamish76@gmail.com>
2024-01-28 22:49:18 +0530

    logic improvise

--- src/App.jsx
+++ src/App.jsx
@@ -34,50 +34,49 @@ function App() {
       // case: if denomarator is 0 so we can't divide
       if (Math.floor((height * height) / 10000) === 0) {
         setBmiValue('BMI is Undefined');
+        setMessage('BMI is Undefined');
       } else {
         const res = (weight / ((height * height) / 10000)).toFixed(2);
         setBmiValue(res);
-      }
 
-      // messages logic
-      const bmi = parseInt(bmiValue);
-      if (bmi > 0 && bmi < 25) setMessage('You are Under weight');
-      else if (bmi >= 25 && bmi < 30) setMessage('You are Healthy weight')
-      else {
-        if(bmiValue !== 'BMI is Undefined') setMessage('You are OverWeight')
+        const bmi = parseInt(bmiValue);
+        if (bmi > 0 && bmi < 25) setMessage('You are Under weight');
+        else if (bmi >= 25 && bmi < 30) setMessage('You are Healthy weight')
+        else {
+          if (!message.length) setMessage('You are Over weight');
+        }
       }
     }
-  }
 
-  const reload = () => {
-    window.location.reload();
-  }
+    const reload = () => {
+      window.location.reload();
+    }
 
-  return (
-    <div className='app'>
-      <div className="container">
-        <h2>BMI Calculator</h2>
-        <form onSubmit={calculateBMI}>
-          <div>
-            <label>Weight (in KG)</label>
-            <input type="number" placeholder='Enter Weight Value' value={weight} onChange={(e) => setWeight(e.target.value)} />
-          </div>
-          <div>
-            <label>Height (in cm) </label>
-            <input type="number" placeholder='Enter Height Value' value={height} onChange={(e) => setHeight(e.target.value)} />
-          </div>
-          <div>
-            <button className='btn' type='submit'>Submit</button>
-            <button className='btn btn-outline' type='submit' onClick={reload}>Reload</button>
-          </div>
-          <div className="msgBox">
-            <h3>Your BMI : {bmiValue}</h3>
-            <p>{message}</p>
-          </div>
-        </form>
+    return (
+      <div className='app'>
+        <div className="container">
+          <h2>BMI Calculator</h2>
+          <form onSubmit={calculateBMI}>
+            <div>
+              <label>Weight (in KG)</label>
+              <input type="number" placeholder='Enter Weight Value' value={weight} onChange={(e) => setWeight(e.target.value)} />
+            </div>
+            <div>
+              <label>Height (in cm) </label>
+              <input type="number" placeholder='Enter Height Value' value={height} onChange={(e) => setHeight(e.target.value)} />
+            </div>
+            <div>
+              <button className='btn' type='submit'>Submit</button>
+              <button className='btn btn-outline' type='submit' onClick={reload}>Reload</button>
+            </div>
+            <div className="msgBox">
+              <h3>Your BMI : {bmiValue}</h3>
+              <p>{message}</p>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
 
-export default App
+  export default App
