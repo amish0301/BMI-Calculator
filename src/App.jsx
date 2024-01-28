0b9db32c7301b7082d9016b099dc334bda926611
amish <pithvaamish76@gmail.com>
2024-01-28 21:51:00 +0530

    logic improvise

--- src/App.jsx
+++ src/App.jsx
@@ -10,21 +10,40 @@ function App() {
   const calculateBMI = (e) => {
     e.preventDefault();
 
-    if(weight > 0 && height > 0) {
-      const res = (weight / Math.floor((height * height) / 10000)).toFixed(2);
-      if(res === 'NaN') setBmiValue('height or weight value cannot be 0');
-      else setBmiValue(res);
+    // if(weight > 0 && height > 0) {
+    //   const res = (weight / Math.floor((height * height) / 10000)).toFixed(2);
+    //   if(res === 'NaN') setBmiValue('height or weight value cannot be 0');
+    //   else setBmiValue(res);
 
-      // message logic
-      if (bmiValue > 0 && bmiValue < 25) {
-        setMessage('You are UnderWeight');
-      } else if (bmiValue >= 25 && bmiValue < 30) {
-        setMessage('You are Healthy Weight');
+    //   // message logic
+    //   if (bmiValue > 0 && bmiValue < 25) {
+    //     setMessage('You are UnderWeight');
+    //   } else if (bmiValue >= 25 && bmiValue < 30) {
+    //     setMessage('You are Healthy Weight');
+    //   } else {
+    //     setMessage('You are OverWeight');
+    //   }
+    // }else {
+    //   alert('Please fill all the details properly');
+    // }
+
+    if (weight === 0 || height === 0) {
+      alert('Please fill valid information');
+    } else {
+
+      // case: if denomarator is 0 so we can't divide
+      if (Math.floor((height * height) / 10000) === 0) {
+        setBmiValue('BMI is Undefined');
       } else {
-        setMessage('You are OverWeight');
+        const res = (weight / ((height * height) / 10000)).toFixed(2);
+        setBmiValue(res);
       }
-    }else {
-      alert('Please fill all the details properly');
+
+      // messages logic
+      const bmi = parseInt(bmiValue);
+      if (bmi < 25) setMessage('You are Under-weight');
+      else if (bmi >= 25 && bmi < 30) setMessage('You are Healthy weight')
+      else setMessage('You are Overweight')
     }
   }
 
@@ -42,11 +61,11 @@ function App() {
         <form onSubmit={calculateBMI}>
           <div>
             <label>Weight (in KG)</label>
-            <input type="text" placeholder='Enter Weight Value' value={weight} onChange={(e) => setWeight(e.target.value)} />
+            <input type="number" placeholder='Enter Weight Value' value={weight} onChange={(e) => setWeight(e.target.value)} />
           </div>
           <div>
             <label>Height (in cm) </label>
-            <input type="text" placeholder='Enter Height Value' value={height} onChange={(e) => setHeight(e.target.value)} />
+            <input type="number" placeholder='Enter Height Value' value={height} onChange={(e) => setHeight(e.target.value)} />
           </div>
           <div>
             <button className='btn' type='submit'>Submit</button>
